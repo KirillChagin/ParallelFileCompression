18e0c2093586c1563a2c826a4c92185c55eda3bb
KirillChagin <kirillchagin@gmail.com>
2018-12-15 19:46:30 +0700

    fixed not saved files

--- ParallelFileCompression/CompressionLib/ICompressor.cs
+++ ParallelFileCompression/CompressionLib/ICompressor.cs
@@ -1,11 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 
 namespace CompressionLib
 {
-    public class Class1
+    public interface ICompressor
     {
+        void Compress(FileInfo fileToCompress, FileInfo compressDestinationFile = null);
+
+        void Decompress(FileInfo fileToDecompress, FileInfo decompressDestinationFile = null);
     }
 }
