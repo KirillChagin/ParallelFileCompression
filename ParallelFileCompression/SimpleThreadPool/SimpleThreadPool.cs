683074f34d8f89028b74b6c50df0911ac6028417
KirillChagin <kirillchagin@gmail.com>
2018-12-15 19:40:38 +0700

    preliminary solution structure

---
+++ ParallelFileCompression/SimpleThreadPool/SimpleThreadPool.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace SimpleThreadPool
+{
+    public static class SimpleThreadPool
+    {
+        public static void QueueWorkItem(ThreadStart threadStart)
+        {
+            
+        }
+    }
+}
